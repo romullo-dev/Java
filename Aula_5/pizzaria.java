89d171ad347fa83cb466f923b6b787df0fb2ca0c
Rômulo França Dev <157732149+romullo-dev@users.noreply.github.com>
2024-06-07 11:42:09 -0300

    commit

--- Aula_5/pizzaria.java
+++ Aula_5/pizzaria.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 class pizzaria {
 
     public static void main (String[] args) {
-        float r_amount, p_amount = 0, s_amount, g , v, total = 0;
-        int option , c = 0;
+        float r_amount, p_amount = 0, s_amount, g , v,  total = 0;
+        int option , c, d_conta = 0, q_amount = 0;
         Scanner scanner = new Scanner(System.in);
+        float total_person = 0;
+        String e_one = "", e_two = "", e_three = "";
 
         do {   
 
@@ -63,31 +65,73 @@ class pizzaria {
 
         }
 
+        switch (d_conta) {
+            case 1:
+                e_one = "Refrigerante";
+                break;
+
+            case 2:
+                e_two = "Pizza grande";
+                break;
+            case 3:
+                e_three = "Sobremesa";
+                break;    
+
+                default:
+                System.out.println(" ");
+                break;
+
+            
+
+
+        }
+
     } while (option != 0);
 
     g = total * 12 / 100;
     v = total + g;
 
+    System.out.println("Total: R$" + total);
+        System.out.println("10% garCom: R$" + g);
+        System.out.println("Total a pagar: R$" + v);
+
+        System.out.println("DESEJA DIVIDIR A SUA CONTA: SIM(1) / NAO(2)");
+        d_conta = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("Quantas pessoas?");
+                q_amount = scanner.nextInt();
+                total_person = v / q_amount;  
+                System.out.println("Total por pessoa: R$" + total_person);
+                break;
+            default:
+                System.out.println("Conta não será dividida.");
+                break;
+        }
     
-    System.out.println(); 
-    System.out.println("     VALOR FINAL");
-    System.out.println("---------------------------------");
-    System.out.println("Total : R$" + total);
-    System.out.println("10% garcom : R$" + g);
-    System.out.println("Total a pagar : R$" + v);
-
-    System.out.println("---------------------------------");
-
-    System.out.println("-----------Desenvolvido por Romulo Franca------------");
-
-
-    
 
 
 
 
-        
-        
+
+        System.out.println();
+        System.out.println("       --------------------CUPOM NAO FISCAL--------------------         ");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("              ------------------PEDIDO-------------------               ");
+        System.out.println();
+        System.out.println("  - " + e_one);
+        System.out.println("  - " + e_two);
+        System.out.println("  - " + e_three);
+        System.out.println();
+        System.out.println("Total : R$" + total);
+        System.out.println("10% garcom : R$" + g);
+        System.out.println("Total a pagar : R$" + v);
+        System.out.println("Valor da conta por pessoa : R$" + total_person +" "+ q_amount + "x");
+        System.out.println(); 
+        System.out.println("-------------------- Desenvolvido por Romulo Franca ---------------------");
+
 
     }
 
