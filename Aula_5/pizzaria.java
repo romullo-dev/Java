a1a6d3f5c1491466af4fb6a471bede2304540538
Rômulo França Dev <157732149+romullo-dev@users.noreply.github.com>
2024-06-07 10:11:34 -0300

    commit

--- Aula_5/pizzaria.java
+++ Aula_5/pizzaria.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 class pizzaria {
 
     public static void main (String[] args) {
-        float option, r_amount, p_amount, s_amount, g , v, total = 0;
+        float r_amount, p_amount = 0, s_amount, g , v, total = 0;
+        int option , c = 0;
         Scanner scanner = new Scanner(System.in);
 
         do {   
 
-        System.out.println ("------------------------- FAÇA O SEU PEDIDO!-------------------------");
+        System.out.println ("------------------------- FACA O SEU PEDIDO!-------------------------");
         System.out.println (" 1 -  Refrigerante ----------- R$ 8,50 ");
         System.out.println (" 2 -  Pizza grande ----------- R$ 30,00");
         System.out.println (" 3 -  Sobremesa    ----------- R$ 10,00");
         System.out.println (" 0 -  Sair");
 
         
-        option = scanner.nextFloat();
+        option = scanner.nextInt();
 
         //REFRI
         if (option == 1) {
@@ -33,6 +34,7 @@ class pizzaria {
             total +=  p_amount * 30.00;
 
             System.out.println (total);
+            
         //SOBREMESA    
         } else if (option == 3) {
             System.out.println (" QUANTIDADE DE SOBREMESA :");
@@ -42,7 +44,23 @@ class pizzaria {
             total +=  s_amount * 10.00;
 
             System.out.println (total);
+
              
+        }
+
+        if (option == 2) {
+
+            System.out.println ("ESCOLHA A COBERTURA DA PIZZA: ");
+            System.out.println ("1 - queijo    ------  R$ 2,50");
+            System.out.println ("2 - presunto  ------  R$ 2,50");
+            System.out.println ("3 - calabresa ------  R$ 2,50");
+            System.out.println ("4 - goiabada  ------  R$ 2,50");
+            System.out.println ("5 - chocolate ------  R$ 2,50");
+            c = scanner.nextInt();
+            total += 2.5 * p_amount;
+            System.out.println (total);
+
+
         }
 
     } while (option != 0);
@@ -51,11 +69,11 @@ class pizzaria {
     v = total + g;
 
     
-    System.out.println(); // Linha em branco para legibilidade
+    System.out.println(); 
     System.out.println("     VALOR FINAL");
     System.out.println("---------------------------------");
     System.out.println("Total : R$" + total);
-    System.out.println("10% garçom : R$" + g);
+    System.out.println("10% garcom : R$" + g);
     System.out.println("Total a pagar : R$" + v);
 
     System.out.println("---------------------------------");
