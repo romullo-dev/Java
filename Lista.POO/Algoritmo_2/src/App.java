924c978bb27c690e41c02d5efbb2b928de6c8961
Rômulo França Dev <romulo.franca03@gmail.com>
2024-06-20 14:56:40 -0300

    commit

---
+++ Lista.POO/Algoritmo_2/src/App.java
@@ -0,0 +1,13 @@
+/* 2 ) Faça um projeto Java orientado a objetos que receba um numero entre 10 e 99  e escreva esse número em ordem crescente e decrescente até 0 e até 100:
+Exemplo:
+       Usuário digitou 27
+Decrescente 26,25,24,23,22,21,20,19.....0
+Crescente 28,29,30,31,32...100
+       Esse projeto deverá te três arquivos o App principal e as classes Crescente e Decrescente.*/
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("ESCREVA UM NUMERO ALEATORIO : ");
+    }
+}
