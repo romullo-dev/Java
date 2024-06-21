ef3a3e4ac1d07160ac2f340b20f3b9bbb57733f4
Rômulo França Dev <romulo.franca03@gmail.com>
2024-06-20 23:58:20 -0300

    commit

---
+++ Lista.POO/Algoritmo_4/src/App.java
@@ -0,0 +1,12 @@
+/*4 ) Criar uma classe Conta, que possua um atributo número e o atributo saldo e os métodos para
+depositar, sacar conferir o saldo. Crie uma classe ContaInterface que possui apenas um método que
+recebe uma conta e mostra um menu para o usuário em que ele pode escolher entre sair do menu,
+depositar um valor, sacar um valor e conferir um saldo. Deve ser possível selecionar essas opções
+diversas vezes até o usuário escolher sair. Crie uma classe UsaConta com um método main que cria
+uma conta e chame o menu de opções.*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Lista.POO/Algoritmo_5/src/App.java
@@ -0,0 +1,8 @@
+/*5 ) Faça um projeto em Java orientado a objetos de um Quiz, perguntas e resposta. Faça no mínimo 5 perguntas com alternativas A,B,C,D, as 
+perguntas podem ser sobre orientação a objetos. Cada resposta certa vale 1 ponto no final mostra a pontuação obtida. */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
