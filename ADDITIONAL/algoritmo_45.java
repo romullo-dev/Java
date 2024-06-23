b023c2d4e5aee27f08b152357f160232cd795235
Rômulo França Dev <romulo.franca03@gmail.com>
2024-06-23 18:41:34 -0300

    commit

--- ADDITIONAL/algoritmo_45.java
+++ ADDITIONAL/algoritmo_45.java
@@ -5,25 +5,37 @@
 import static java.lang.System.out;
 import java.util.Scanner;
 
-public class c {
+public class algoritmo_45 {
     public static void main(String[] args) {
         Scanner s = new Scanner (System.in);
-        int C = 0;
+        int C = 0, II = 0, HH = 0, c =0, PP = 0, PPorce, C_P=0;
         
         do {   
             out.println("Escreva sua idade :  ");
             int I = s.nextInt();
             out.println("Escreva a sua altura : ");
             float H = s.nextFloat();
+            out.println("Escreva o seu peso : ");
+            float P = s.nextFloat();
 
-            C++;    
+            C++; 
 
-        } while (C <= 5);
+            if (I >= 50) {
+                II++;
+            } else if (I > 9 && I < 21){
+                HH += H;
+                c++;
+                HH = HH /c;
+            } else if (P <= 40) {
+                PP++;
+                PPorce = PP * C/100;
+            }
 
+        } while (C <= 1);
 
-        out.println("QUANTIDADE DE PESSOAS MAIOR DE 50 ANOS : ");
-        out.println("A média das Alturas das pessoas com idade entre 10 e 20 anos : ");
-        out.println("Porcentagem das pessoas com peso inferior a 40 quilos entre todas as pessoas analisadas : ");
+        out.println("QUANTIDADE DE PESSOAS MAIOR DE 50 ANOS : " + II);
+        out.println("A média das Alturas das pessoas com idade entre 10 e 20 anos : " + HH);
+        out.println("Porcentagem das pessoas com peso inferior a 40 quilos entre todas as pessoas analisadas : " + PP);
 
     }
     
