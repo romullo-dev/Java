182b544c9fc6801c2b3f577c9950f5a2fbfe940e
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2024-06-25 22:06:20 -0300

    Atualizar o algoritmo_45.java

--- ADDITIONAL/4_dezena/algoritmo_45.java
+++ ADDITIONAL/4_dezena/algoritmo_45.java
@@ -1,4 +1,4 @@
-/*45. Faça um programa que receba a idade, altura e o peso de 25 pessoas, Calcule e mostre:
+by/*45. Faça um programa que receba a idade, altura e o peso de 25 pessoas, Calcule e mostre:
  A quantidade de pessoas com idade superior a 50 anos;
  A média das Alturas das pessoas com idade entre 10 e 20 anos
  A porcentagem das pessoas com peso inferior a 40 quilos entre todas as pessoas analisadas.*/
@@ -28,7 +28,7 @@ public class algoritmo_45 {
                 HH = HH /c;
             } else if (P <= 40) {
                 PP++;
-                PPorce = PP * C/100;
+                PPorce = PP * C/100 +1 ;
             }
 
         } while (C <= 1);
