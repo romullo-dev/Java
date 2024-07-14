a6fb4d8a336988fff750ae68f7733e6bacb75f24
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2024-07-14 17:02:04 -0300

    Atualizar o teste.java

--- Aula_1/teste.java
+++ Aula_1/teste.java
@@ -1,11 +1,5 @@
-class teste {
+class Helo {
     public static void main(String[] args){
-        System.out.println("***TESTE***");
-        System.out.println("Opcao 1");
-        System.out.println("Opcao 2");
-        System.out.println("Opcao 3");
-        System.out.println("Sair 0");
-        System.out.println("***Fim***");
-        
+        System.out.println("Hello, mundo!");
     }
 }
